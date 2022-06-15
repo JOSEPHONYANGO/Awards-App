80fc6d8365369b9c721bbd8b66a4b88976d7aa69
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-15 19:17:37 +0300

    updte profile

--- prorater/forms.py
+++ prorater/forms.py
@@ -17,4 +17,9 @@ class CreateUserForm(UserCreationForm):
 class NewProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['name', 'description', 'screenshot', 'repository_url', 'live_url']        
+        fields = ['name', 'description', 'screenshot', 'repository_url', 'live_url'] 
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']               
