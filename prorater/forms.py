2c70b7c9b34388b658dfc961f5a12fdb6ca37173
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-15 18:56:24 +0300

    user form

--- prorater/forms.py
+++ prorater/forms.py
@@ -3,3 +3,13 @@ from django.forms import ModelForm, fields
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .models import Profile, Project, Review
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name','username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.pop("autofocus", None)
