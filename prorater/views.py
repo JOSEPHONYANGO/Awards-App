5c2d10d294ae879b528433a77040cf56aee89974
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-15 18:26:55 +0300

    apiview

--- prorater/views.py
+++ prorater/views.py
@@ -104,4 +104,18 @@ def project(request, project_id):
 
     context = {'project': project, 'reviews':reviews, 'review_form':review_form}
 
-    return render(request, 'project.html', context)    
+    return render(request, 'project.html', context) 
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+
+        return Response(serializers.data)
+
+class ProjectsList(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectsSerializer(all_projects, many=True)
+
+        return Response(serializers.data)       
