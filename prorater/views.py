1351248b806e6fc164c6fe31c366522951f4eee8
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-15 18:14:52 +0300

    register

--- prorater/views.py
+++ prorater/views.py
@@ -52,3 +52,20 @@ def login_user(request):
 
     context = {}
     return render(request, 'auth/login.html', context)
+
+def register_user(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    else:
+        form = CreateUserForm()
+        title = 'New Account'
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+
+    context = {'form': form, 'title': title}
+    return render(request, 'auth/register.html', context)
