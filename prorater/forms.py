cf3de8a20f503cba292ea4d1dd6956f0ec6b3f31
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-15 18:55:04 +0300

    forms

---
+++ prorater/forms.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import User
+from django.forms import ModelForm, fields
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import Profile, Project, Review
