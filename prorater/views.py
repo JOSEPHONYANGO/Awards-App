aec2b9eb9048a9baf9916b68628c14afd11235d7
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-15 17:10:24 +0300

    import

--- prorater/views.py
+++ prorater/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import CreateUserForm, NewProjectForm, UpdateProfileForm, ReviewForm
+from django.contrib import messages
+from .models import Project, Review
+from django.contrib.auth.decorators import login_required
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Profile
+from .serializer import ProfileSerializer, ProjectsSerializer
+from wwwrate import serializer
 
 # Create your views here.
