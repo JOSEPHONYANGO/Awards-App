1b8001af9255746c38861fe0b15bb151e529dcbf
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-16 10:25:48 +0300

    urls2

--- prorater/urls.py
+++ prorater/urls.py
@@ -1,2 +1,13 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('auth/login/', views.login_user,name='login'),
+    path('auth/register/', views.register_user,name='register'),
+    path('profile/', views.profile,name='profile'),
+    path('project/<projectid>', views.profile,name='profile'),
+    path('api/profiles/', views.ProfileList.as_view()),
+    path('api/projects/', views.ProjectsList.as_view())
+    
+]
