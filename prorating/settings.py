3fe3fd5c19eb90768ef3d869060272206c65b8c8
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-17 08:21:02 +0300

    new changes

--- prorating/settings.py
+++ prorating/settings.py
@@ -115,6 +115,8 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+
 cloudinary.config(
    cloud_name = 'bishopjoe',
    api_key = '535635979348921',
@@ -164,17 +166,18 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
 
 
-
-
-
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR, 'media'
 
-django_heroku.settings(locals())
+
+
